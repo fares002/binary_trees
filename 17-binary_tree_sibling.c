a1800a1e55ef2e5cd3ad3858df9f529b2f1546f7
Abeermohsien <aboorakhalil993@gmail.com>
2024-02-29 16:48:03 -0800

    $

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling of
- *
- * Return: pointer to the sibling node, or NULL if no sibling
+ * binary_tree_sibling - finds sibling 
+ * @node: pointer
+ * Return: pointer
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - finds the uncle of a node
- * @node: pointer to the node to find the uncle of
+ * binary_tree_uncle - the uncle of a node
+ * @node: pointer
  *
- * Return: pointer to the uncle node, or NULL if no uncle
+ * Return: pointer
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
