b1b5b96e93a49d46b3fd73456c14c63dad2752b5
fares002 <fares@example.com>
2024-02-29 16:10:50 -0800

    done

--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-
+/* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -46,9 +46,9 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-
-
+/* UTILITIES */
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
